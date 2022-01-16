425c65a8632f3f5e806f65faa7cea288cfa11a9d
lvlshanks <shankerssy@gmail.com>
2022-01-16 17:40:45 +0800

    added Page Not Found page

--- src/pages/NotFound/NotFound.tsx
+++ src/pages/NotFound/NotFound.tsx
@@ -1,3 +1,29 @@
-const NotFound = () => <div>NotFound</div>;
+import { Box, Button, Typography } from '@mui/material';
+import { useNavigate } from 'react-router-dom';
+
+const NotFound = () => {
+  const navigate = useNavigate();
+
+  return (
+    <Box display="flex" alignItems="center" flexDirection="column">
+      <div>
+        <Typography variant="h1" fontWeight="bold">
+          404
+        </Typography>
+      </div>
+      <div>
+        <Typography variant="h5" fontWeight="bold">
+          PAGE NOT FOUND
+        </Typography>
+      </div>
+      <Box pb={3} />
+      <Button onClick={() => navigate('/')}>
+        <Typography variant="h6" fontWeight="bold">
+          Take me home
+        </Typography>
+      </Button>
+    </Box>
+  );
+};
 
 export default NotFound;
